61d2f24313ce71f664f40779d3fffc8259a47f34
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-05-24 11:10:22 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-indigo-600 text-4xl font-bold">Database Backup</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
