1df095245fb2779b62346f96f2af8a68c7724465
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-05-24 11:57:39 +0200

    feat: Setup Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-function App() {
+import {
+  AppShell,
+  Burger,
+  Footer,
+  Header,
+  MantineProvider,
+  MediaQuery,
+  Navbar,
+  Text,
+  Title,
+  useMantineTheme,
+} from '@mantine/core';
+import { useState } from 'react';
+
+export default function App() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
   return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <h1 className="text-indigo-600 text-4xl font-bold">Database Backup</h1>
-      </div>
-    </>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <AppShell
+        padding="md"
+        navbar={
+          <Navbar hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }} height={500} p="xs">
+            <Text>Navbar content</Text>
+          </Navbar>
+        }
+        footer={
+          <Footer height={60} p="md">
+            Application footer
+          </Footer>
+        }
+        header={
+          <Header height={{ base: 50, md: 70 }} p="md">
+            <div className="flex items-center h-full">
+              <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.dark[8]}
+                  mr="xl"
+                ></Burger>
+              </MediaQuery>
+
+              <Title>Header</Title>
+            </div>
+          </Header>
+        }
+      >
+        <Text>Login page</Text>
+      </AppShell>
+    </MantineProvider>
   );
 }
-
-export default App;
