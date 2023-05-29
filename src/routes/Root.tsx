f55c15d2f72c92648e3a3c07b9aa83cd5cebc290
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-05-29 13:17:14 +0200

    feat: Temporally remove navbar

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -13,12 +13,10 @@ import {
 import { useEffect, useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useLocalStorage } from 'react-use';
-import NavBarLink from '../components/NavBarLink';
 import { useGlobalToken } from '../utilities/globals';
 
 function Root() {
   const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
   const [token, setToken] = useGlobalToken();
   const [localToken, _setLocalToken, removeLocalToken] = useLocalStorage('token');
   const navigate = useNavigate();
@@ -40,27 +38,11 @@ function Root() {
   return (
     <AppShell
       padding="md"
-      navbar={
-        <Navbar hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }} p="xs" className="h-full">
-          <Navbar.Section grow mt="xs">
-            <Stack spacing="sm">
-              {!token ? <NavBarLink label="Login" href="/" /> : null}
-              <NavBarLink label="Schedules" href="/schedules" />
-            </Stack>
-          </Navbar.Section>
-        </Navbar>
-      }
       header={
         <Header height={{ base: 50, md: 70 }} p="md">
           <div className="flex items-center h-full">
             <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.dark[8]}
-                mr="xl"
-              />
+              <Burger opened={false} onClick={() => {}} size="sm" color={theme.colors.dark[8]} mr="xl" />
             </MediaQuery>
             <Group position="apart" className="w-full">
               <Title order={2}>Database Backup</Title>
